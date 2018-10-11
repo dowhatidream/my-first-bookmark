1e7f6ec01f2236392765cf5f9c776cf640632b7f
dowhatidream <rkfgusch@naver.com>
2018-10-12 05:44:39 +0900

    Changed ...

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -13,20 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 # URL 패턴에 따라 뷰를 호출할 예정(아직 해당 뷰 코드는 작성 전 상태)
-from bookmark.views import BookmarkLV, BookmarkDV
+# from bookmark.views import BookmarkLV, BookmarkDV
 # from bookmark.views import * # 모든 뷰 항목을 일괄 임포트
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('bookmark.urls')),
-
+    # url(r'^admin/', admin.site.urls),
     # # 북마크 앱을 위한 클래스 기반 뷰
     # # /bookmark/ 요청을 처리할 뷰 클래스를 BookmarkLV로 지정하고, URL 패턴 이름 지정
     # url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
     # # /bookmark/숫자/ 요청을 처리할 뷰 클래스를 BookmarkDV로 지정하고, URL 패턴 이름 지정
     # url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+
+    url(r'^bookmark/$', views.BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', views.BookmarkDV.as_view(), name='detail'),
+    # tabular list
+    url(r'^bookmark_t_CBV/$', views.BookmarkLV.as_view(), name='index_t_CBV'),
+    url(r'^bookmark_t_FBV/$', views.tabularBookmark, name='index_t_FBV'),
 ]
