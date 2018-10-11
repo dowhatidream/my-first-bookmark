f356ef5c0da2dbb30b5f3c6279c4fa5e23cb0870
dowhatidream <rkfgusch@naver.com>
2018-10-12 05:51:21 +0900

    Changed ...

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from bookmark.models import Bookmark
 
+def tabularBookmark(request):
+    bookmarks = Bookmark.objects.all().order_by('id')
+    return render(request, 'bookmark/tabular_list.html', {'bookmarks': bookmarks})
+
 # 북마크 테이블의 전체 레코드 리스트 출력을 위한 뷰
 class BookmarkLV(ListView):
     model = Bookmark
